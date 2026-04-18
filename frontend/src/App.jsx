b5f0f4821c04e8fa4fbf160adc4de24408caa62d
JuliaPTe <tellejulia7@gmail.com>
2026-04-18 07:04:06 +0200

    Jobber på profilkomponentet

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
-
 import './App.css'
+import Header from './Components/Header'
+import MemberJulia from './components/memberJulia'
 
 function App() {
 
   return (
-    <></>
+    // Lagt til komponentene på forsiden slik at jeg kan se det mens jeg jobber
+    <section>
+      <Header />
+      <MemberJulia />
+    </section>
   )
 }
 
